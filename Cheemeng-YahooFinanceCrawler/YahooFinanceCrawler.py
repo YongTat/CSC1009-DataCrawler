44a70fb0897e0940aeed348fe5a6347739ad9dbc
Cheemeng97 <34565690+Cheemeng97@users.noreply.github.com>
2021-03-07 15:41:28 +0800

    Update YahooFinanceCrawler.py
    Able to skip/ignore invalid symbols

--- Cheemeng-YahooFinanceCrawler/YahooFinanceCrawler.py
+++ Cheemeng-YahooFinanceCrawler/YahooFinanceCrawler.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
 import scrapy
+import json
 
 #MongoDB libraries
 from pymongo import MongoClient
@@ -61,11 +62,11 @@ for i in range(0,400,100):
                 #ccLinks.append("https://sg.finance.yahoo.com/quote/"+str(ccSymbol)+"/history?p=interval=1d&filter=history&frequency=1d&includeAdjustedClose=true")
 
 #Extract historical data for each CC by using Symbol
-iccSymbol = 0
+iccSymbol = 0   
 while iccSymbol < len(ccSymbols):
     #Url of Historical data for each Cyrptocurrencies on YahooFinance
     CurrentCCUrl = "https://sg.finance.yahoo.com/quote/"+str(ccSymbols[iccSymbol]) +"/history?p=interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
-    
+  
     #HTTP request to the Url
     CurrentCCr= requests.get(CurrentCCUrl)
     
@@ -76,18 +77,32 @@ while iccSymbol < len(ccSymbols):
     #Find Historical table from html
     HistoricalData_table = CurrentCCSoup.find("table", attrs={"data-test":"historical-prices"})
     
-    #Find all row from the Historical table
-    HistoricalData_data = HistoricalData_table.tbody.find_all("tr")
+    #If there is no historical data
+    if HistoricalData_table is None:
+        print("No Historical data")
+        iccSymbol +=1
+    else:
+        #Find all row from the Historical table
+        HistoricalData_data = HistoricalData_table.tbody.find_all("tr")
+        
+        # Get all the headings of Lists
+        datas = []
+        for tr in HistoricalData_table.tbody.find_all("tr"):
+            for td in tr.find_all("td"):
+                #datas.append(ccSymbols[iccSymbol],td.text)
+                datas.append(td.text)
+            print(CurrentCCUrl)
+            print(datas)
+        iccSymbol += 1
+
+    
+    # Json
     
 
+# for i in ccLinks:
+#     print(i, end='')
+#     print()
 
-
-    # Get all the headings of Lists
-    datas = []
-    for tr in HistoricalData_table.tbody.find_all("tr"):
-        for td in tr.find_all("td"):
-            #datas.append(ccSymbols[iccSymbol],td.text)
-            datas.append(td.text)
-        print(CurrentCCUrl)
-        print(datas)
-    iccSymbol += 1
+# df = pd.read_html(str(HistoricalData_table))
+# print(ccSymbols[iccSymbol])
+# print(df[0].to_json(orient='records'))
