0468ff08ef358419fe3dad275358be2c0d528cd7
YongTat <59991299+YongTat@users.noreply.github.com>
2021-03-09 18:20:34 +0800

    changes for front end use
    -Limit to last 100 data
    -added new link to get collection list

--- api.py
+++ api.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource,Api
+
+# use if running front and back end on same device
+from flask_cors import CORS
+
 import pymongo
 from bson.json_util import dumps
 # import python module from teams mates below here
@@ -8,18 +12,20 @@ from YongTat_YFinance import StockGetter
 # init flask app / api
 app = Flask(__name__)
 api = Api(app)
-
+# use if running front and back end on same device
+CORS(app)
 # Login Variables
 login = []
 with open("config.txt", "r") as f:
     for line in f:
         login.append(line.strip())
 client = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.vk8mu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(login[0],login[1]))
-db = client["stocks"]
 
 # Code to run when /stocks/ticker is ran
 class Stock(Resource):
     def get(self, ticker):
+        # connect to stocks database
+        db = client["stocks"]
         # Get all collection of stocks
         existing_list = db.list_collection_names()
         try:
@@ -36,18 +42,28 @@ class Stock(Resource):
         finally:
             # get all documents
             data = db[ticker]
-            json_return = list(data.find())
+            json_return = list(data.find().sort("Date",-1).limit(100))
             return dumps(json_return)
+
+class StocksList(Resource):
+    def get(self):
+        db = client["stocks"]
+        return db.list_collection_names()
 
 # Code to run when /twitter/ticker is ran
 class Twitter(Resource):
+    # connect to twitter database
+    db = client["twitter"]
     pass
 
 # Code to run when /reddit/ticker is ran
 class Reddit(Resource):
+    # connect to reddit database
+    db = client["reddit"]
     pass
 
 api.add_resource(Stock, "/stocks/<string:ticker>")
+api.add_resource(StocksList, "/stockslist")
 api.add_resource(Twitter, "/twitter/<string:handle>")
 api.add_resource(Reddit, "/reddit/<string:user>")
 
