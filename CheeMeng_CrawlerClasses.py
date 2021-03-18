f3b23b5eb3fb1a8575833483094571cc6cd8ae54
Cheemeng97 <34565690+Cheemeng97@users.noreply.github.com>
2021-03-18 11:30:38 +0800

    Update CheeMeng_CrawlerClasses.py
    Validate values such as url, data values

--- CheeMeng_CrawlerClasses.py
+++ CheeMeng_CrawlerClasses.py
@@ -2,12 +2,13 @@
 import requests
 from bs4 import BeautifulSoup
 import yfinance as yf
+import validators
 
 #Mongodb Database
 import pymongo
 
 # Array
-stockIndustries = ["energy",
+stockIndustries = [# "energy",
                     # "financial",
                     # "healthcare",
                     # "industrials",
@@ -32,19 +33,39 @@ stockSymbol = []
 class getIndustries():
     # Set Base url
     def __init__(self, industry):
-        self.baseUrl =  "https://sg.finance.yahoo.com/industries/" + industry
+        try:
+            if type(industry) != str:
+                raise Exception("Value must be string")
+        except Exception:  
+            print("Input must be a string type")
+            print("Converting input to string")
+            industry = str(industry)
+        finally:
+            self.baseUrl =  "https://sg.finance.yahoo.com/industries/" + industry
 
 class createParser(getIndustries):
     # Using beautiful soup to create parser
     def getParser(self):
-        #HTTP request to the Url
-        r= requests.get(self.baseUrl)
-        #Creating BS object and instruct BS to use 'lxml' parser
-        data=r.text
-        soup=BeautifulSoup(data, 'lxml')
-        return soup
+        #Check if baseUrl is a valid Url
+        validUrl = validators.url(self.baseUrl)
+        #If is a valid url
+        if validUrl == True:
+            #HTTP request to the Url
+            r= requests.get(self.baseUrl)
+            try:
+                if r.status_code == 404:
+                    raise Exception("HTTP URL not found")
+            except Exception:
+                print("Page not found")
 
-
+            finally:
+                #Creating BS object and instruct BS to use 'lxml' parser
+                data=r.text
+                soup=BeautifulSoup(data, 'lxml')
+                return soup
+        #If is invalid url
+        else:
+            print("Invalid url")
 
 ############ Crawl Historical Data from Yahoo Finance ###########
 class setSymbol():
@@ -56,20 +77,72 @@ class crawlHistoricalData(setSymbol):
     # Crawl historical data                    
     def getHistoricalData(self):
             print(self.currentSymbol)
-            #Access ticker data
             stock = yf.Ticker(self.currentSymbol)
-            #Get max period of historical data 
             data = stock.history(period="max")
-            #Request for those values
             data = data[["Open","High","Low","Close","Volume"]]
             data.reset_index(inplace=True)
             data = data.to_dict("records")
-            #Set historical data into a list
             data = list(data)
+
+            # Check if data is in correct datatype
+            i = 0
+            while i < len(data):
+                openValue = data[i]["Open"] #float
+                openRes = isinstance(openValue, str)
+
+                highValue = data[i]["High"] #float
+                highRes = isinstance(highValue, str)
+
+                lowValue = data[i]["Low"] #float
+                lowRes = isinstance(lowValue, str)
+                
+                closeValue = data[i]["Close"] #float
+                closeRes = isinstance(closeValue, str)
+
+                volumeValue = data[i]["Volume"] #int
+                volumeRes = isinstance(volumeValue, str)
+
+                # Open value must be in float
+                # If value is a string(true)
+                if str(openRes) == "True":
+                    openValue = 0.0
+                # If value if not float
+                elif type(openValue) != float:
+                    print("Open: Value must be in float type")
+                    openValue = float(openValue)
+                
+                # High value must be in float
+                # If value is a string(true)
+                if str(highRes) == "True":
+                    highValue = 0.0
+                # If value is not float
+                elif type(highValue) != float:
+                    print("High: Value must be in float type")
+                    highValue = float(highValue)
+
+                # Low value must be in float
+                #If value is a string(true)
+                if str(lowRes) == "True":
+                    lowValue = 0.0
+                # If value is not float
+                elif type(lowValue) != float:
+                    print("Low: Value must be in float type")
+                    lowValue = float(lowValue)
+
+                # Close value must be in float
+                # If value is string(true)
+                if str(closeRes) == "True":
+                    closeValue = 0.0
+                # If value is not float
+                elif type(closeValue) != float:
+                    print("Close: Value must be in float type")
+                    closeValue = float(closeValue)
+                
+                i += 1
+
             return data
 
-
-
+    
 ############ Use Beautiful soup parser ###########
 ############ Crawl stocks from Yahoo Finance ###########
 class crawlSymbol():
@@ -77,7 +150,7 @@ class crawlSymbol():
         self.i = i
         self.soup = soup
 
-    #Get stock symbols from webpage
+    # Get stock symbols from webpage
     def getSymbol(self):
         #Scrap all symbols from table
         for listing in self.soup.find_all('tr', attrs={'data-reactid':self.i}):
@@ -88,7 +161,6 @@ class crawlSymbol():
                     #Add new symbol into array
                     stockSymbol.append(Symbol)
         return stockSymbol
-
 
 
 ############ connection to MongoDb ###########
@@ -104,8 +176,7 @@ class connectionToMongoDB(getDatabaseName):
         with open("config.txt", "r") as f:
             for line in f:
                 login.append(line.strip())
-        client = 
-            pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.vk8mu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(login[0],login[1]))
+        client = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.vk8mu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(login[0],login[1]))
 
         # Connect to stocks database
         db = client[self.database]        
@@ -178,3 +249,26 @@ def crawlIndustryStocks(db, existing_list):
             else:
                 print("No Historical data")
         i+=1
+
+def main():
+    # retrieve existing stock names from dababase
+    db = connectionToMongoDB("stock")
+    db = db.getCurrentDb()
+    existing_list = db.list_collection_names
+
+
+    crawlTopStock(db, existing_list)
+    crawlIndustryStocks(db, existing_list)
+
+    # # retrieve existing stock names from dababase
+    # db = YFinanceCrawler.connectionToMongoDB("stock")
+    # db = db.getCurrentDb()
+    # existing_list = db.list_collection_names
+
+    # YFinanceCrawler.crawlTopStock(db, existing_list)
+    # YFinanceCrawler.crawlIndustryStocks(db, existing_list)
+
+
+
+if __name__ == "__main__":
+    main()
